04c5c9f44438ba49c92623e2ccf3b89fe5db05ce
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 10:41:39 -0600

    yo

--- streamer.py
+++ streamer.py
@@ -4,23 +4,33 @@ import json
 import requests
 import time
 
+cache = []
 arrarray = []
 
 ser = serial.Serial('/dev/cu.usbmodem621', 9600)
 
-while(true):
-	try:
+while(True):
+    try:
         r = requests.get(
-    	    "https://sheltered-tundra-18217.herokuapp.com/coordinates")
-    
-    except requests.ConnectionError, e:
-    	print e
-    	time.sleep(5)
-    	continue
+             "https://sheltered-tundra-18217.herokuapp.com/coordinates")
+    except Exception as error:
+        print(error)
+        time.sleep(5)
+        continue
+
+    jray = json.dump(r, separators=[',', ',', ':'])
+    newray = json.JSONDecoder(jray)
+
+    if cache == null or newray == cache:
+        cache = newray
+        arraray.append(newray)
+    else:
+        time.sleep(5)
+        continue
 
     arrarray.append(json.dump(r,skipkeys='NULL'))	
     for array in arrarray:
-	    for coordinate in array
+	    for coordinate in array:
 	        changed_coor = coor.coordinate_change(coordinate)
-		    ser.write(str(changed_coor.c_1) + ' ')
-		    ser.write(str(changed_coor.c_2) + '\n')
+            ser.write(str(changed_coor.c_1) + ' ')
+            ser.write(str(changed_coor.c_2) + '\n')
