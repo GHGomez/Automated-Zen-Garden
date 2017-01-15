75e43d795be8e794e8e0de4b5bf1bcfe257de768
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 01:41:25 -0600

    Fixed coordinate algorithm

--- coordinateshift.py
+++ coordinateshift.py
@@ -18,8 +18,9 @@ class Coordinate:
 #Changes Coordinates from rectangular to dual polar
 def coordinate_change(coro):
     L3 = ma.sqrt((coro.c_1 ** 2) + (coro.c_2 ** 2))
-    rho = ma.atan((coro.c_1 ** 2) + (coro.c_2 ** 2))
-    theta = rho + ma.acos(((L2 ** 2) - (L1 ** 2) - (L3 ** 2))/(-2 * L1 * L3))
+    rho = ma.atan((coro.c_2 ** 2) / (coro.c_1 ** 2))
+    theta = rho + ma.acos(((L2 ** 2) - (L1 ** 2) - (L3 ** 2))
+        /(-2 * L1 * L3))
     phi = ma.acos(((L3 ** 2) - (L2 ** 2) - (L1 ** 2))/(-2 * L1 * L2))
     return Coordinate(theta * 180 / ma.pi, phi * 180 / ma.pi)
 
