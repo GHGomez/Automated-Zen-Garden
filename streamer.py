27a6fd9d0525a41a5c4c8e9799719b655949a51f
Max Dubrinsky <msdubrin@mtu.edu>
2017-01-15 06:04:24 -0600

    spelling

--- streamer.py
+++ streamer.py
@@ -9,6 +9,6 @@ ser = serial.Serial('/dev/cu.usbmodemFA131', 9600)
 
 for array in arrarray:
 	for coordinate in array:
-		changed_coor = coordinate_chage(coordinate)
+		changed_coor = coor.coordinate_change(coordinate)
 		ser.write(str(changed_coor.c_1) + ' ')
 		ser.write(str(changed_coor.c_2) + '\n')
