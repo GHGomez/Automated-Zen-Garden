6beeb5753e7842c8c577ab730fb73fd0fbff04f0
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 00:02:55 -0600

    yay

--- coordinateshift.py
+++ coordinateshift.py
@@ -19,7 +19,7 @@ class Coordinate:
 def coordinate_change(coro):
     L3 = ma.sqrt((coro.c_1 ** 2) + (coro.c_2 ** 2))
     rho = ma.atan((coro.c_1 ** 2) + (coro.c_2 ** 2))
-    theta = rho + ma.acos(((L2 ** 2) + (L1 ** 2) - (L3 ** 2))/(-2 * L1 * L3))
+    theta = rho + ma.acos(((L2 ** 2) - (L1 ** 2) - (L3 ** 2))/(-2 * L1 * L3))
     phi = ma.acos(((L3 ** 2) - (L2 ** 2) - (L1 ** 2))/(-2 * L1 * L2))
     return Coordinate(theta * 180 / ma.pi, phi * 180 / ma.pi)
 
