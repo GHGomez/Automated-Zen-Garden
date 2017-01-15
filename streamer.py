fa9b127a25ace02834d8c03dbc1cb8a106a68ce2
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 04:29:23 -0600

    Cast coordinates as ints

--- streamer.py
+++ streamer.py
@@ -1,6 +1,13 @@
 import coordinateshift as coor
 import serial
 
+arrarray = []
+arrarray.append([coor.Coordinate(0.25, 0.25), coor.Coordinate(0.75, 0.75)
+	, coor.Coordinate(0.25, 0.25)])
 
-# ser = serial.Serial('/dev/ttyUSB0', 9600)
-print str(coor.Coordinate(0.25, 0.25))
+ser = serial.Serial('/dev/ttyUSB0', 9600)
+
+for array in arrarray:
+	for coordinate in array:
+		stringcor = str(coordinate)
+		ser.write(bytes(coordinate))
