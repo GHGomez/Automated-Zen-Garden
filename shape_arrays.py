0ba56afde2286dc43da8ea0db68c17ce92d38660
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 01:21:17 -0600

    Added new csv

--- shape_arrays.py
+++ shape_arrays.py
@@ -1,4 +1,19 @@
-import coordinateshift
+import coordinateshift as coor
 
-squarearray = [Coordinate(0.25, 0.25), Coordinate(0.75, 0.25), 
-Coordinate(0.75, 0.75), Coordinate(0.25, 0.75)]
+squarearray = [coor.Coordinate(0.25, 0.25), coor.Coordinate(0.75, 0.25), 
+coor.Coordinate(0.75, 0.75), coor.Coordinate(0.25, 0.75), 
+coor.Coordinate(0.25, 0.25)]
+
+coor.convert_corarray(squarearray, 'squarearray.csv')
+
+trianglearray = [coor.Coordinate(0.20, 0.20), coor.Coordinate(0.80, 0.20)
+, coor.Coordinate(0.50, 0.80), coor.Coordinate(0.20, 0.20)]
+
+coor.convert_corarray(trianglearray, 'trianglearray.csv')
+
+resetarray = [coor.Coordinate(0.01, 0.01)]
+
+coor.convert_corarray(resetarray, 'reset.csv')
+
+
+
