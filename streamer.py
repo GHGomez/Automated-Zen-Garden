55fa322ebd896686bc48cab4ccf7e982f0fdaca3
Gregory Howlett-Gomez <ghowlettgomez@gmail.com>
2017-01-15 07:04:28 -0600

    Yay

--- streamer.py
+++ streamer.py
@@ -1,14 +1,26 @@
 import coordinateshift as coor
 import serial
+import json
+import requests
+import time
 
 arrarray = []
-arrarray.append([coor.Coordinate(0.25, 0.25), coor.Coordinate(0.75, 0.75)
-	, coor.Coordinate(0.25, 0.25)])
 
 ser = serial.Serial('/dev/cu.usbmodem621', 9600)
 
-for array in arrarray:
-	for coordinate in array:
-		changed_coor = coor.coordinate_change(coordinate)
-		ser.write(str(changed_coor.c_1) + ' ')
-		ser.write(str(changed_coor.c_2) + '\n')
+while(true):
+	try:
+        r = requests.get(
+    	    "https://sheltered-tundra-18217.herokuapp.com/coordinates")
+    
+    except requests.ConnectionError, e:
+    	print e
+    	time.sleep(5)
+    	continue
+
+    arrarray.append(json.dump(r,skipkeys='NULL'))	
+    for array in arrarray:
+	    for coordinate in array
+	        changed_coor = coor.coordinate_change(coordinate)
+		    ser.write(str(changed_coor.c_1) + ' ')
+		    ser.write(str(changed_coor.c_2) + '\n')
